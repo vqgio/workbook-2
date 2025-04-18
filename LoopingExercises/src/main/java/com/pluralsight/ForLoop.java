f9a3239044416e78e1569f372eec793814e8f0af
Gio <giovannizavala82@gmail.com>
2025-04-18 11:40:30 -0500

    ForLoop and WhileLoop EX1

---
+++ LoopingExercises/src/main/java/com/pluralsight/ForLoop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class ForLoop {
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 10; i > 0 ; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        System.out.println("Launch!");
+    }
+}
---
+++ LoopingExercises/src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+
+        //while method
+
+//        while (count < 5) {
+//            System.out.println("I love java!");
+//            count++;
+//        }
+        //do while method
+
+//        do {
+//            System.out.println("I love java!");
+//            count++;
+//        }   while (count < 5);
+        //fori method
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i love java!");
+        }
+        //break statement
+        //if(count == 2){
+        //   break;
+        //}
+
+    }
+}
