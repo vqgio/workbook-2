124e126690ce8283a6f0a4debe3c40e86f741c1f
Gio <giovannizavala82@gmail.com>
2025-05-31 19:58:32 -0500

    exercise 1 completed.

---
+++ VehicleInventory/src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Vehicle {
+    public static void main(String[] args) {
+        long vehicleId;
+        String makeModel;
+        String color;
+        int odometerReading;
+        float price;
+
+        
+
+    }
+}
